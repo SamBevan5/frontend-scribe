fb817b5b3470f7e4616bc77e0c49f797ae8761dd
Sam Bevan <samueleganbevan@gmail.com>
2020-07-05 17:43:29 -0400

    added images and styled homepage

--- scribe/src/components/Home.js
+++ scribe/src/components/Home.js
@@ -35,7 +35,33 @@ export default (props) => {
                     </div>
                 </div>
                 <div className="App__main-content">
-
+                    <div className="App__main-content__infographics">
+                        <div className="App__main-content__infographics__icon">
+                            <img src="/legend-home.png" alt="quill-book"></img>
+                            <h1>Stay Organized. Become Efficient</h1>
+                        </div>
+                        <div className="App__main-content__infographics__1">
+                            <div className="App__main-content__infographics__1__text">
+                                <h1>Keep track of thoughts. Set Goals. Be more productive.</h1>
+                                <h4>Scribe helps you through the whole process from idea to action.</h4>
+                                <a href="/register">TRY IT OUT ></a>
+                            </div>
+                            <div className="App__main-content__infographics__1__image">
+                                <img src="/homepagediagram.png" alt="diagram-homepage"></img>
+                            </div>
+                        </div>
+                        <div className="App__main-content__infographics__2">
+                            <div className="App__main-content__infographics__2__image">
+                                <img src="/anywherehomepage.png" alt="map-homepage"></img>
+                            </div>
+                            <div className="App__main-content__infographics__2__text">
+                                <h1>Create a note anywhere, anytime</h1>
+                                <h4>With cross-platform access scribe let's you stay organized at home or on the go.</h4>
+                                <a href="/register">TRY IT OUT ></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div></div>
                 </div>
 
             </div>
